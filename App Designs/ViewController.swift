32d4b18eaa24828ac7f6823d5946ac0704e735d6
varunbhalla19 <varunbhalla19@gmail.com>
2022-09-04 23:13:29 +0530

    changed nav back button

--- App Designs/ViewController.swift
+++ App Designs/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let customBackButton = UIBarButtonItem(title: " ", style: .plain, target: nil, action: nil)
+        customBackButton.tintColor = .white
+        navigationItem.backBarButtonItem = customBackButton
+        
     }
 
 
